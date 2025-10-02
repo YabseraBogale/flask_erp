45dfb111b4a99c670f007e09df130d297ae5d853
YabseraBogale <yabserapython@gmail.com>
2025-10-02 20:05:05 +0300

    first frontend and feature that will the module

--- app.py
+++ app.py
@@ -147,9 +147,10 @@ def login():
         employee_id=request.form["employee_id"]
         password=request.form["password"]
         emoplyee=db.session.query(Employee).filter(Employee.employee_id==employee_id).first()
-        #bcrypt.checkpw(password.encode("utf-8"),)
-        print(type(emoplyee.password))
-        return "ok"
+        is_vaild=bcrypt.checkpw(password.encode("utf-8"),emoplyee.password)
+        if is_vaild==True:
+            return redirect("/dashboard")
+        return redirect("/login")
 
     return render_template("login.html")
 

cce233a45e709ba2276e15e65392e733564ebd19
YabseraBogale <yabserapython@gmail.com>
2025-11-03 18:49:21 +0300

    updating the database

---
+++ database.sql
@@ -0,0 +1,32 @@
+Insert Into Location(location) 
+values  ("Addis Ababa"),("Adama"),("Dire Dawa"),("Mekelle"),
+        ("Gondar"),("Bahir Dar"),("Hawassa"),
+        ("Jimma"),("Harar"),("Dessie"),("Shashamane"),
+        ("Asella"),("Debre Markos"),("Gambela"),("Jijiga"),("Arba Minch"),("Dilla");
+
+Insert Into Unit(unit) 
+values ("Piece"),("Dozen"),("Package"),("Gram"),
+        ("Kilogram"),("Ton"),("Milliliter"),("Liter"),
+        ("Gallon"),("Barrel"),("Millimeter"),("Centimeter"),
+        ("Meter"),("Roll"),("Sheet"),("Bottle"),("Can");
+
+Insert Into Category(Category) 
+values ("Food"),("Furniture"),("Safety Equipment"),
+        ("Chemical"),("Sanitary"),("Spare Part"),
+        ("Stationery"),("Electronics"),
+        ("Accessory"),("Clothing"),("Constraction Material"),("Pad");
+
+Insert Into Department(Department) 
+values ("Human Resources"),("Finance"),("Sales"),
+        ("Procurement"),("Administration"),("Store");
+
+Insert Into Currency(Currency) 
+values ("USD"),("EUR"),("JPY"),
+        ("GBP"),("CNY"),("CHF"),
+        ("CAD"),("AUD"),("SGD"),
+        ("HKD"),("ETH");
+
+Insert Into Subcategory(Subcategory) 
+values ("Fixed Assest"),("Moving");
+        
+        

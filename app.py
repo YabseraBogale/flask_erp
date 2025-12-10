9157b250145f1d641e1094d81c09e6f19f5e467b
YabseraBogale <yabserapython@gmail.com>
2025-12-10 13:58:22 +0300

    ok

--- app.py
+++ app.py
@@ -1247,6 +1247,7 @@ def budget_list():
 
 @app.route("/finanical_data")
 @login_required
+@cache.cached(timeout=600)
 def finanical_data():
     try:
         if session["department_name"]=="Finance" or session["department_name"]=="Administration":
